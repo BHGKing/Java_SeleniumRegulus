b76698075306048c0740d961334c8889af59732f
BHGKing <josecantu111@gmail.com>
2023-06-18 19:59:32 -0500

    Did task for Week05 and added target folder to gitignore

--- main/Java/Java/JoseTask/Week05/UniqueChar.java
+++ main/Java/Java/JoseTask/Week05/UniqueChar.java
@@ -1,6 +1,45 @@
 package JoseTask.Week05;
 
 public class UniqueChar {
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBCCCDEF";
+
+        System.out.println("String = " + str);
+
+        System.out.println("Unique Characters from String = " + uniqueChars(str));
+
+
+    }
+
+    public static String uniqueChars(String str){
+
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            boolean isUnique = true;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (i != j && currentChar == str.charAt(i)){
+
+                    isUnique = false;
+                    break;
+
+                }
+
+            }
+
+            if (isUnique){
+                unique += str.charAt(i);
+            }
+
+        }
+        return unique;
+    }
+
 }
 /*
 Write a return method that can find the unique characters from the String
