b4235954f231cc12a44d1986a6275ca5c8f1c5a7
Milena <milena86radojicic@gmail.com>
2023-06-22 12:32:08 -0500

    Added Week1-5

---
+++ main/Java/Java/MilenaTask/week2/Task03.java
@@ -0,0 +1,11 @@
+package MilenaTask.week2;
+
+public class Task03 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
---
+++ main/Java/Java/MilenaTask/week3/PrimeNumber.java
@@ -0,0 +1,33 @@
+package MilenaTask.week3;
+
+public class PrimeNumber {
+
+        public static boolean isPrime(int number) {
+
+
+            for (int i = 2; i < number; i++) {
+
+                if (number % i == 0) {
+                    System.out.println(number + " is not a prime number");
+                    return false;
+                }
+
+            }
+
+            System.out.println(number + " is prime number");
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+
+            isPrime(23);
+
+
+        }
+
+
+    }
+
+
+
