d6e29e599b1e071bb8cd2faa8184f271f0007104
maiyada <maiyada.hasan12@gmail.com>
2023-06-21 22:07:41 -0500

    Added week05

---
+++ main/Java/Java/maiyadaTask/maiyadaTask/week05_JavaTask/Reverse.java
@@ -0,0 +1,24 @@
+package Java.maiyadaTask.maiyadaTask.week05_JavaTask;
+
+public class Reverse {
+
+    //Write a function that can reverse a String
+    // Ex: Reverse("ABCD"); ==> DCBA
+
+
+    public static String StrReverse(String str){
+
+        String reverse ="";
+        for (int i = str.length()-1; i >=0 ; i--) {
+           reverse += str.charAt(i);
+        }
+
+
+        return  reverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StrReverse("ABCD"));
+
+    }
+}
