2458b67287fa131fe3ccf4732f4d20b4b7058b42
BHGKing <josecantu111@gmail.com>
2023-06-24 00:10:50 -0500

    updated and added week06 code

--- main/Java/Java/JoseTask/Week04/sameLetter.java
+++ main/Java/Java/JoseTask/Week04/sameLetter.java
@@ -1,48 +1,33 @@
 package JoseTask.Week04;
 
+import java.util.Arrays;
+
 public class sameLetter {
 
     public static void main(String[] args) {
-        String str1 = "abc";
-        String str2 = "cab";
-        System.out.println(same(str1, str2));  // Output: true
 
-        str1 = "abc";
-        str2 = "abb";
-        System.out.println(same(str1, str2));  // Output: false
+        System.out.println(same("abc", "cab"));
+        System.out.println(same("abc", "abb"));
+
     }
 
-    public static boolean same(String str1, String str2) {
-        // Check if the lengths of the strings are different
-        if (str1.length() != str2.length()) {
-            return false;
-        }
+    public static boolean same(String a, String b) {
 
-        // Create character frequency arrays for both strings
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        char[] ch1 = a.toCharArray();
+        char[] ch2 = b.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        String a1="", a2="";
+        for(char each: ch1)
+            a1 +=each;
 
-        // Count the frequencies of characters in the first string
-        for (int i = 0; i < str1.length(); i++) {
-            char c = Character.toLowerCase(str1.charAt(i));
-            freq1[c - 'a']++;
-        }
+        for(char each: ch2)
+            a2 +=each;
 
-        // Count the frequencies of characters in the second string
-        for (int i = 0; i < str2.length(); i++) {
-            char c = Character.toLowerCase(str2.charAt(i));
-            freq2[c - 'a']++;
-        }
+        return  a1.equals(a2) ;
 
-        // Compare the character frequencies
-        for (int i = 0; i < 26; i++) {
-            if (freq1[i] != freq2[i]) {
-                return false;
-            }
-        }
-
-        return true;
     }
+
 }
 /*
 Write a return method that checks if a string is built out of the same letters as another string.
