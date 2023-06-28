49d344b5e3a6ae44ab48236c7b591558e108fe0e
BHGKing <josecantu111@gmail.com>
2023-06-27 20:22:53 -0500

    initial commit

---
+++ main/Java/Java/JoseTask/GitPractice/Practice1.java
@@ -0,0 +1,12 @@
+package JoseTask.GitPractice;
+
+public class Practice1 {
+    public static void main(String[] args) {
+
+        String p = "Hello World";
+
+        System.out.println(p);
+
+
+    }
+}
