10255e35da32c50f2d26b50973816a18936166dc
fouad <karamifouad2@hotmail.com>
2023-06-27 22:02:35 -0400

    made more chnages

--- main/Java/Java/Git_practice/PractiingGit.java
+++ main/Java/Java/Git_practice/PractiingGit.java
@@ -3,5 +3,7 @@ package Git_practice;
 public class PractiingGit {
     public static void main(String[] args) {
         System.out.println("github is confusing");
+        System.out.println("hello");
     }
+
 }
