ad28c1e1db676430362b7f523b97d9dc0fccb09b
Saim_Cydeo <at.crumrine@gmail.com>
2023-07-13 22:32:47 -0500

    Message to Milena

--- main/Java/Java/Practicing_Git.java
+++ main/Java/Java/Practicing_Git.java
@@ -2,7 +2,8 @@ public class Practicing_Git {
     public static void main(String[] args) {
 
         System.out.println("Get Ready to create Conflict - Jose");
-
+        System.out.println("Milena");
+        System.out.println("Are you ready for our project ton start?/");
 
 
 
